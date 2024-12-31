3276a127c4939bbc23d6add3256980e835a20a09
Jonathon Duerig <>
2024-12-31 16:35:43 -0700

    Updated config examples, fleshed out make-data-lists.

--- make-data-lists.py
+++ make-data-lists.py
@@ -1,8 +1,21 @@
 # Accept a train or val list on standard input and split it into many
 # lists based on the duration of the clips.
+#
+# --wav is the directory of the wav files to check
+# --out is the output directory where a bunch of segment lists will be spawned
 
 import soundfile as sf
-import re, sys
+import argparse, pathlib, re, sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--wav", default="wav/")
+parser.add_argument("--out", default="out/")
+args = parser.parse_args()
+
+wavdir = pathlib.Path(args.wav)
+outdir = pathlib.Path(args.out)
+if not outdir.exists():
+    outdir.mkdir(parents=True)
 
 already = {}
 time_bins = {}
@@ -17,20 +30,18 @@ def length_to_bin(length):
 for line in sys.stdin:
     fields = line.split("|")
     if fields[0].strip() in already:
-        print("DUPLICATE", fields[0].strip())
+        sys.stderr.write("DUPLICATE " + fields[0].strip())
     else:
         already[fields[0].strip()] = True
-    audio, sample_rate = sf.read("wav/"
-                                 + fields[0].strip())
+    audio, sample_rate = sf.read(str(wavdir / fields[0].strip()))
     binkey = length_to_bin(audio.shape[0] // 300)
     if binkey not in time_bins:
         time_bins[binkey] = []
-    time_bins[binkey].append((fields[0].strip(), fields[1].strip()))
+    time_bins[binkey].append((fields[0].strip(), fields[1].strip(), fields[2].strip()))
 
 for key in sorted(time_bins.keys()):
     #print(str(key) + ":", len(time_bins[key]))
-    filename = ("train/list-%d.txt" % key)
-    with open(filename, "w") as f:
+    filename = outdir / ("list-%d.txt" % key)
+    with filename.open("w") as f:
         for item in time_bins[key]:
-            text = re.sub(r"([↗↘])", r" \1 ", item[1])
-            f.write("%s|%s|0\n" % (item[0], text))
+            f.write("%s|%s|%s\n" % (item[0], item[1], item[2]))
