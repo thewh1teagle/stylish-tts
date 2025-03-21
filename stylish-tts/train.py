32ec6277df6d887990e62accb6b336fa030e0dce
Jonathon Duerig <>
2025-03-21 09:08:09 -0600

    Get total loss if mel is not available for progress bar

--- stylish-tts/train.py
+++ stylish-tts/train.py
@@ -287,9 +287,15 @@ def train_val_loop(train: TrainContext, should_fast_forward=False):
                 if next_log is not None:
                     logs.append(next_log)
                     if loss is None:
-                        loss = next_log.metrics["mel"]
+                        if "mel" in next_log.metrics:
+                            loss = next_log.metrics["mel"]
+                        else:
+                            loss = next_log.total()
                     else:
-                        loss = loss * 0.9 + next_log.metrics["mel"] * 0.1
+                        if "mel" in next_log.metrics:
+                            loss = loss * 0.9 + next_log.metrics["mel"] * 0.1
+                        else:
+                            loss = loss * 0.9 + next_log.total() * 0.1
                 if len(logs) >= train.config.training.log_interval:
                     progress_bar.clear() if progress_bar is not None else None
                     combine_logs(logs).broadcast(train.manifest, train.stage)
