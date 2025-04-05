39e4ffa117a79492eeb0ea2e710f22b5a2853c21
Jonathon Duerig <>
2025-04-05 03:14:30 +0000

    Fix sampling for pre_textual stage

--- stylish-tts/models/pitch_energy_predictor.py
+++ stylish-tts/models/pitch_energy_predictor.py
@@ -1,4 +1,5 @@
 import torch
+from torch.nn import functional as F
 from xlstm import (
     xLSTMBlockStackConfig,
     mLSTMBlockConfig,
@@ -96,11 +97,13 @@ class PitchEnergyPredictor(torch.nn.Module):
         x = self.shared(prosody.transpose(-1, -2))
         x = self.prepare_projection(x)
         F0 = x.transpose(-1, -2)
+        F0 = F.interpolate(F0, scale_factor=2, mode="nearest")
         for block in self.F0:
             F0 = block(F0, style)
         F0 = self.F0_proj(F0)
 
         N = x.transpose(-1, -2)
+        N = F.interpolate(N, scale_factor=2, mode="nearest")
         for block in self.N:
             N = block(N, style)
         N = self.N_proj(N)
